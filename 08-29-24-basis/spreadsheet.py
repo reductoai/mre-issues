3ae1e87e9df400ba9cd7a98a8b2bace3c7b401f4
Raunak <raunak@reducto.ai>
2024-08-29 21:30:02 +0000

    update to do extract instead

--- 08-29-24-basis/spreadsheet.py
+++ 08-29-24-basis/spreadsheet.py
@@ -4,13 +4,29 @@ import os
 
 api_key = os.environ["REDUCTO_API_KEY"]
 
-upload_form = requests.post("https://v1.api.reducto.ai/upload?extension=.xlsx",
-                            headers={"Authorization": f"Bearer {api_key}"}).json()
+upload_form = requests.post(
+    "https://v1.api.reducto.ai/upload?extension=.xlsx",
+    headers={"Authorization": f"Bearer {api_key}"},
+).json()
 
 requests.put(upload_form["presigned_url"], data=open("./sample.xlsx", "rb"))
 
-response = requests.post("https://v1.api.reducto.ai/parse",
-                         json={"document_url": upload_form["file_id"]},
-                         headers={"Authorization": f"Bearer {api_key}"})
+response = requests.post(
+    "https://v1.api.reducto.ai/extract",
+    json={
+        "document_url": upload_form["file_id"],
+        "schema": {
+            "type": "object",
+            "properties": {
+                "message": {
+                    "type": "string",
+                    "description": "The main content of the message.",
+                }
+            },
+            "required": ["message"],
+        },
+    },
+    headers={"Authorization": f"Bearer {api_key}"},
+)
 
 print(response.json())
